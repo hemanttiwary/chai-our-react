4f0eb1a3c0017a6ac699e38f9550f90f41fe01ab
hemanttiwary <hemant.appu@gmail.com>
2023-10-16 15:27:12 +0530

    new development

---
+++ basic-app/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>CHAI OUR REACT </h1>
+  );
+}
+
+export default App;
---
+++ vite-basic/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+function App() {
+   return (
+    <h1>CHAI OUR CODE WITH VITE </h1>
+  )
+}
+
+export default App
